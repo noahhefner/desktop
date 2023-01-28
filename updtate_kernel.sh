bb7fe523e3a1f418a4a3e6df35192258fc5a2dd4
Noah Hefner <noah.hefner127@gmail.com>
2023-01-28 12:13:00 -0500

    preferences and sources files

--- updtate_kernel.sh
+++ updtate_kernel.sh
@@ -6,26 +6,13 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# Add unstable repositories
-echo "deb http://deb.debian.org/debian unstable main contrib non-free" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://deb.debian.org/debian unstable main contrib non-free" | sudo tee -a /etc/apt/sources.list
+# Update packages sources
+rm /etc/apt/sources.list
+cp ./sources.list /etc/apt/sources.list
 
-# TODO: Make all repositories contrib and non-free
-
-# Create apt pinning file
-tee /etc/apt/preferences<<EOF
-Package: *
-Pin: release a=bullseye
-Pin-Priority: 500
-
-Package: linux-image-amd64
-Pin:release a=unstable
-Pin-Priority: 1000
-
-Package: *
-Pin: release a=unstable
-Pin-Priority: 100
-EOF
+# Update apt pinning file
+rm /etc/apt/preferences
+cp ./preferences /etc/apt/preferences
 
 # Update and upgrade
 apt update && sudo apt upgrade -y
