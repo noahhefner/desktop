4b9826f3196ae5ec33abf25a9abc9ef2181cb6a4
Noah Hefner <noah.hefner127@gmail.com>
2023-01-29 14:52:57 -0500

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
-
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
