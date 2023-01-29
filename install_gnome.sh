ca56f6c1397f503ad8ec94f7d1e3017e8b327258
Noah Hefner <noah.hefner127@gmail.com>
2023-01-29 14:03:06 -0500

    broke gnome script, fix now

--- install_gnome.sh
+++ install_gnome.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 # Install Gnome desktop environment
-nix-env -iA \
-  nixpkgs.gnome.gnome-session \
-  nixpkgs.gnome.gnome-shell \
-  nixpkgs.gnome.gnome-terminal \
-  nixpkgs.gnome.gnome-backgrounds \
-  nixpkgs.gnome.gnome-applets \
-  nixpkgs.gnome.gnome-control-center \
-  nixpkgs.gnome.mutter \ 
-  nixpkgs.gjs
+nix-env -iA nixpkgs.gnome.gnome-session nixpkgs.gnome.gnome-shell nixpkgs.gnome.gnome-terminal nixpkgs.gnome.gnome-backgrounds nixpkgs.gnome.gnome-applets nixpkgs.gnome.gnome-control-center nixpkgs.gnome.mutter nixpkgs.gjs
 
 # Print instructions
 echo "Gnome has been installed. Restart now."
